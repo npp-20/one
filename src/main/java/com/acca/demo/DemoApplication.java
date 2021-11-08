38dc68541b6761517eeb87f4278f162d4370ba09
nie <1776204783@qq.com>
2021-11-08 16:51:17 +0800

    hot2

--- src/main/java/com/acca/demo/DemoApplication.java
+++ src/main/java/com/acca/demo/DemoApplication.java
@@ -9,8 +9,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 //		System.out.println("git for master code change");
 //		System.out.println("git for hot-fix code change");
-//		System.out.println("git for hot-fix code change2");
-//		System.out.println("git for website code change");
+		System.out.println("git for hot-fix code change2");
+		System.out.println("git for website code change");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
