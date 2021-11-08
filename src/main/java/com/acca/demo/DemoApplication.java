125e7844d12c874da6c4f15bc24840b944982751
nie <1776204783@qq.com>
2021-11-08 16:46:06 +0800

    hot

--- src/main/java/com/acca/demo/DemoApplication.java
+++ src/main/java/com/acca/demo/DemoApplication.java
@@ -7,9 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("git for master code change");
-		System.out.println("git for hot-fix code change");
-		System.out.println("git for hot-fix code change2");
+//		System.out.println("git for master code change");
+//		System.out.println("git for hot-fix code change");
+//		System.out.println("git for hot-fix code change2");
 //		System.out.println("git for website code change");
 		SpringApplication.run(DemoApplication.class, args);
 	}
